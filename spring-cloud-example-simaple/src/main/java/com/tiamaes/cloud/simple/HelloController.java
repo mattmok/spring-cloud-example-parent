8166156ed7edbda628a9a31b735469e73137771f
mattmok <chenlili0224@gmail.com>
2017-06-14 12:02:17 +0800

    commit

--- spring-cloud-example-simaple/src/main/java/com/tiamaes/cloud/simple/HelloController.java
+++ spring-cloud-example-simaple/src/main/java/com/tiamaes/cloud/simple/HelloController.java
@@ -2,6 +2,8 @@ package com.tiamaes.cloud.simple;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,16 +19,23 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "hello")
 public class HelloController {
 	
+	
+	@PreAuthorize("hasRole('ADMIN') OR hasRole('USER')")
 	@ApiOperation(value = "hello value", notes = "hello notes", response = String.class)
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public @ResponseBody String hello(HttpSession session, @CurrentUser Authentication user) {
-		
 //		try {
 //			Thread.sleep(10 * 1000);
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
-//		System.out.println(user);
+		return "Hello world, " + (user == null ? session.getId() : user.getPrincipal()) + "!";
+	}
+	
+	@Secured("ADMIN")
+	@ApiOperation(value = "hello value", notes = "hello notes", response = String.class)
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public @ResponseBody String admin(HttpSession session, @CurrentUser Authentication user) {
 		return "Hello world, " + (user == null ? session.getId() : user.getPrincipal()) + "!";
 	}
 }
